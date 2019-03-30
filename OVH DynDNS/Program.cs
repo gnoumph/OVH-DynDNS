4ddcba81f01a31e964cd335248b99ccc0555d210
Gaby <tirfortgabriel@free.fr>
2019-03-30 16:12:47 +0100

    release

---
+++ OVH DynDNS/Program.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json;
+using Ovh.Api;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace OVH_DynDNS
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Variables.
+            string publicIp = "";
+            string configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\OVH DynDNS\\";
+            string configFile = configPath + "config.json";
+            string logFile = configPath + "ovh_dyndns.log";
+            StringBuilder logs = new StringBuilder();
+
+            // Check log size.
+            if (File.Exists(logFile) && new FileInfo(logFile).Length >= 1048576) // 1 Mo.
+            {
+                File.Delete(logFile);
+            }
+
+            logs.Append("===================\n");
+            logs.Append(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss\n\n"));
+
+            // Get public IP.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.ipify.org/");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.UserAgent = "OVH DynDNS";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                publicIp = reader.ReadToEnd();
+                logs.Append("Current public IP is: " + publicIp + ".\n");
+            }
+
+            // Create AppData directory.
+            if (!Directory.Exists(configPath))
+            {
+                Directory.CreateDirectory(configPath);
+                logs.Append("Directory created: " + configPath + ".\n");
+            }
+
+            // Create config file.
+            if (!File.Exists(configFile))
+            {
+                OvhConfig newConfig = new OvhConfig
+                {
+                    LastPublicIp = publicIp
+                };
+
+                File.WriteAllText(configFile, JsonConvert.SerializeObject(newConfig));
+                logs.Append("File created: " + configFile + ".\n");
+            }
+
+            // Get config.
+            OvhConfig config = JsonConvert.DeserializeObject<OvhConfig>(File.ReadAllText(configFile));
+            logs.Append("Last public IP is: " + config.LastPublicIp + ".\n");
+
+            // Check config.
+            if (
+                !string.IsNullOrEmpty(config.OvhRegion) &&
+                !string.IsNullOrEmpty(config.ApplicationKey) &&
+                !string.IsNullOrEmpty(config.ApplicationSecret) &&
+                !string.IsNullOrEmpty(config.ConsumerKey) &&
+                !string.IsNullOrEmpty(config.ZoneName)
+            )
+            {
+                // Change IP for all records of
+                // the DNS zone if needed.
+                if (publicIp != config.LastPublicIp)
+                {
+                    logs.Append("Current IP is different from last IP, apply changes.\n");
+                    Client client = new Client(config.OvhRegion, config.ApplicationKey, config.ApplicationSecret, config.ConsumerKey);
+                    int[] recordsIds = client.Get<int[]>("/domain/zone/" + config.ZoneName + "/dynHost/record");
+
+                    foreach (int recordId in recordsIds)
+                    {
+                        OvhRecord record = client.Get<OvhRecord>("/domain/zone/" + config.ZoneName + "/dynHost/record/" + recordId);
+                        record.Ip = publicIp;
+                        client.Put("/domain/zone/" + config.ZoneName + "/dynHost/record/" + recordId, "{\"ip\": \"" + record.Ip + "\"}");
+                        logs.Append("Subdomain changed: " + record.SubDomain + ".\n");
+                    }
+
+                    // Refresh DNS zone.
+                    client.Post("/domain/zone/" + config.ZoneName + "/refresh", "");
+                    logs.Append("Configuration updated.\n");
+
+                    // Update config.
+                    config.LastPublicIp = publicIp;
+                    File.WriteAllText(configFile, JsonConvert.SerializeObject(config));
+
+                    // Send SMS.
+                    if (
+                        !string.IsNullOrEmpty(config.SmsUser) &&
+                        !string.IsNullOrEmpty(config.SmsPass)
+                    )
+                    {
+                        WebRequest.Create("https://smsapi.free-mobile.fr/sendmsg?user=" + config.SmsUser + "&pass=" + config.SmsPass + "&msg=Public%20IP%20change%20detected%21%0ANew%20IP%20is%20" + publicIp).GetResponse();
+                    }
+                }
+                else
+                {
+                    logs.Append("Current IP is similar to last IP, no changes needed.\n");
+                }
+            }
+            else
+            {
+                logs.Append("WARNING! Config not found!\n");
+                logs.Append("Please add your config in " + configFile + "!\n");
+            }
+
+            logs.Append("===================\n\n\n");
+            File.AppendAllText(logFile, logs.ToString());
+            logs.Clear();
+
+            return 0;
+        }
+    }
+
+    public class OvhRecord
+    {
+        public string Ip { get; set; }
+        public string SubDomain { get; set; }
+    }
+
+    public class OvhConfig
+    {
+        public string OvhRegion { get; set; }
+        public string ApplicationKey { get; set; }
+        public string ApplicationSecret { get; set; }
+        public string ConsumerKey { get; set; }
+        public string ZoneName { get; set; }
+        public string SmsUser { get; set; }
+        public string SmsPass { get; set; }
+        public string LastPublicIp { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,111 @@
 # OVH-DynDNS
+
+This program allow you to automatically update your DNS zone when your public IP is renewed.  
+It works with OVH and can send you a SMS when it's done (works only with Free Mobile).
+
+## What's your needs?
+
+### Visual Studio 2017 Community Edition
+
+Download and install Visual Studio 2017 Community Edition to be able to open and compile the solution. The Community Edition is free and meets your needs.  
+https://visualstudio.microsoft.com/
+
+### OVH token
+
+First, you need an API token from OVH. It allows you to manage your account from any custom app.
+
+Go to https://api.ovh.com/createToken/ and set your account informations, set **Validity** to _Unlimited_ then set **Rights** like this:
+
+ * **PUT** /domain/zone/{zoneName}/dynHost/record/*
+ * **POST** /domain/zone/{zoneName}/refresh
+ * **GET** /domain/zone/{zoneName}/dynHost/record
+ * **GET** /domain/zone/{zoneName}/dynHost/record/*
+
+Of course, replace **{zoneName}** with... your zone name.  
+Create your keys, note them.
+
+### Free Mobile key (optionnal)
+
+This part is fully optionnal, and is needed only if you want to receive a notification by SMS when your DNS zone is updated. It supports **only** Free Mobile.
+
+Go to https://mobile.free.fr/moncompte/, then click on **Gérer mon compte** and **Mes options**. Scroll down then activate the **Notifications par SMS** option.  
+Note your credentials.
+
+### Config file
+
+The config file is a simple JSON file.
+
+Create an **OVH DynDNS** folder in **%AppData%** (usually ```C:\Users\{username}\AppData\Roaming```).  
+In this folder, create a **config.json** file, then write this JSON object:
+
+```
+{
+  "OvhRegion":"ovh-eu",
+  "ApplicationKey":"{ovhApplicationKey}",
+  "ApplicationSecret":"{ovhApplicationSecret}",
+  "ConsumerKey":"{ovhConsumerKey}",
+  "ZoneName":"{ovhZoneName}",
+  "SmsUser":"{freeMobileSmsUser}",
+  "SmsPass":"{freeMobileSmsPass}"
+}
+```
+
+As said before, the **SmsUser** and **SmsPass** are fully optionnal and can be ignored.
+
+### Set a custom script to get your current public IP (optionnal)
+
+If you look at the source code, you'll notice at line 31 a call to https://api.ipify.org/.  
+It's needed to get the actual public IP. But if you don't want to use an external ressource that you can't control, just add this small PHP script on your web server then replace the address by your own.
+
+```
+<?php
+echo $_SERVER['REMOTE_ADDR'];
+?>
+```
+
+That's it!
+
+## Compiling the application
+
+Now you are ready to compile the app.
+
+Open the **OVH DynDNS.sln** file. If it's not the case, select **Release** in the dropdown list (**Debug** should be selected by default) in the top toolbar.  
+Now you can compile the program by pressing **F6**.
+
+A new folder is created in your project directory. Navigate to ```...\OVH DynDNS\bin\Release``` then copy everything where you want to store the program.
+
+Note the path.
+
+## Set a scheduled task
+
+Now you have to create a new scheduled task to start the program automatically periodically.
+
+Open the tasks manager then create a new task.
+
+In **General** tab, set the task **Name**, check **Run whether user is logged on or not** and **Run with highest privileges**, and in **Configure for** select your Windows version.
+
+In **Triggers** tab, click on **New...**. Begin the task **At startup**, repeat task every **1 minutes** (or everything else you want) for a duration of **Indefinitely**.
+
+In **Actions** tab, click on **New...** then on **Browse...**. Navigate to your application path, then select **OVH DynDNS.exe**.
+
+In **Conditions** tab, uncheck everything.
+
+In **Settings** tab, check **Run task as soon as possible after a scheduled start is missed** and uncheck **Stop the task if it runs longer than**.
+
+Validate, then you're ready!
+
+## How to improve? What's next?
+
+ * Add more registars support
+ * Add more SMS providers support
+ * Create the config file from the program
+ * Automatically create the scheduled task (or use another way to check periodically)
+ * Create an installer for non-developper users
+ * Find a better way to get current public IP (and don't use an external URL)
+ * Create an UI
+ * Probably a lot of things!
+
+## Why this program?
+
+Because my ISP want my money every months for a static public IP, and my server is hosted on my personnal computer.  
+So I wrote this small program, and I want to share it with everyone, even if I don't think I'll continue to work on it for now.
